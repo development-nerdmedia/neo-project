800fbe2b73614f40eb4be61559981e390510b435
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-05-06 13:13:33 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -7,9 +7,28 @@ window.addEventListener("scroll", function () {
     }
 });
 
+MyApp = {
+    slider: {
+        init: function () {
+            const root = document.documentElement;
+            const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+            const marqueeContent = document.querySelector("ul.marquee-content");
+
+            root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+            for (let i = 0; i < marqueeElementsDisplayed; i++) {
+                marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+            }
+        }
+    }
+}
+if ($('.slider ').length > 0) {
+    MyApp.slider.init();
+}
+
 $('.slider-proyectos').slick({
     centerMode: true,
-    centerPadding: '60px',
+    centerPadding: '0px',
     slidesToShow: 3,
     responsive: [
         {
@@ -32,3 +51,41 @@ $('.slider-proyectos').slick({
         }
     ]
 });
+
+$('.slider-servicios').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    dots: false,
+    slidesToShow: 2.57,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
+
