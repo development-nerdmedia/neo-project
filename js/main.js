6509f20b296bc0e1a05c5121f80e4f4f99275716
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-05-10 18:07:51 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -20,10 +20,53 @@ MyApp = {
                 marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
             }
         }
+    },
+    sliderClientes: {
+        init: function () {
+            const root2 = document.documentElement;
+            const marqueeElementsDisplayed2 = getComputedStyle(root2).getPropertyValue("--marquee-elements-displayed2");
+            const marqueeContent2 = document.querySelector("ul.marquee-content2");
+
+            root2.style.setProperty("--marquee-elements2", marqueeContent2.children.length);
+
+            for (let i = 0; i < marqueeElementsDisplayed2; i++) {
+                marqueeContent2.appendChild(marqueeContent2.children[i].cloneNode(true));
+            }
+        }
+    },
+    categorias: {
+        init: function () {
+            const enlaces = document.querySelectorAll('#categorias2 a');
+            $('.item-category').hide();
+            const categoryMain = document.querySelector('#categorias2 a.select').innerHTML.toLowerCase();
+            // console.log(categoryMain.innerHTML);
+            console.log(categoryMain);
+            $(`.item-category[data-categoria="${categoryMain}"]`).show(500);
+
+            enlaces.forEach((elemento) => {
+                elemento.addEventListener('click', (evento) => {
+                    evento.preventDefault();
+                    enlaces.forEach((enlace) => enlace.classList.remove('select'));
+                    evento.target.classList.add('select');
+
+                    const categoria = evento.target.innerHTML.toLowerCase(); /* para saber la categoria del menu donde estas*/
+                    console.log(categoria);
+                    $(`.item-category`).not(`[data-categoria="${categoria}"]`).hide(500);
+                    $(`.item-category[data-categoria="${categoria}"]`).show(500);
+                });
+            });
+        }
     }
 }
 if ($('.slider ').length > 0) {
     MyApp.slider.init();
+}
+if ($('.sliderClientes ').length > 0) {
+    MyApp.sliderClientes.init();
+}
+
+if ($('#categorias2 ').length > 0) {
+    MyApp.categorias.init();
 }
 
 $('.slider-proyectos').slick({
@@ -65,8 +108,6 @@ $('.slider-servicios').slick({
             settings: {
                 slidesToShow: 3,
                 slidesToScroll: 3,
-                infinite: true,
-                dots: true
             }
         },
         {
@@ -83,9 +124,54 @@ $('.slider-servicios').slick({
                 slidesToScroll: 1
             }
         }
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
     ]
 });
 
+$('.slider-fotos').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+/* popup */
+document.addEventListener("click", (e) => {
+    if (e.target.closest(".items-category")) {
+        togglePortfolioPopup();
+        // document.querySelector(".item-popup").scrollTo(0, 0);
+        // portfolioItemDetails(e.target.parentElement);
+    }
+})
+
+function togglePortfolioPopup() {
+    document.querySelector(".item-popup").classList.toggle("open");
+    // document.body.classList.toggle("hide-scrolling");
+    // document.querySelector(".main").classList.toggle("fade-out");
+}
+
+document.querySelector(".btn-close").addEventListener("click", togglePortfolioPopup);
