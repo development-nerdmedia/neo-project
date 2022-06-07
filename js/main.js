df93dcf5006ad0ab1e32e54bd25e9421c9322245
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-06-07 12:16:52 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -139,15 +139,15 @@ MyApp = {
                         {
                             breakpoint: 1024,
                             settings: {
-                                slidesToShow: 3,
-                                slidesToScroll: 3,
+                                slidesToShow: 1,
+                                slidesToScroll: 1,
                             }
                         },
                         {
                             breakpoint: 600,
                             settings: {
-                                slidesToShow: 2,
-                                slidesToScroll: 2
+                                slidesToShow: 1,
+                                slidesToScroll: 1
                             }
                         },
                         {
